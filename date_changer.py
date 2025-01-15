996949c64bfb0c3b505aebfc3065af6c3adf3b7d
Daleshin93 <aleshin_d_e@mail.ru>
2025-01-15 11:26:39 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,7 @@
+
+created_date = input('Дата создания заметки: ')
+issue_date = input('Введите дату дедлайна: ')
+
+
+print(f'Дата создания заметки: {created_date[0:5]}')
+print(f'Дата дедлайна: {issue_date[0:5]}')
