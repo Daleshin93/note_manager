6438bc79d6f8a0aff76684888dab2343aae2c9bd
Daleshin93 <aleshin_d_e@mail.ru>
2025-01-26 10:46:47 +0300

    Add files via upload

--- update_status.py
+++ update_status.py
@@ -1,5 +1,5 @@
 status = input('Статус: ')
-note = ['username', 'titles', 'content', status, 'temp_created_date', 'temp_issue_date']
+print(f'Текуший статус: {status}')
 statuses = ['В работе', 'Отложено', 'Выполнено']
 
 while True:
@@ -8,28 +8,19 @@ while True:
                        f'2- {statuses[1]}\n'
                        f'3- {statuses[2]}\n')
     if new_status in statuses:
-        note[3] = new_status
+        status = new_status
         print(f'\nСтатус заметки изменён на: {new_status}.')
         break
-    elif new_status == '1':
-        note[3] = statuses[0]
-        print(f'\nСтатус заметки изменён на: {statuses[0]}.')
-        break
-    elif new_status == '2':
-        note[3] = statuses[1]
-        print(f'\nСтатус заметки изменён на: {statuses[1]}.')
-        break
-    elif new_status == '3':
-        note[3] = statuses[2]
-        print(f'\nСтатус заметки изменён на: {statuses[2]}.')
-        break
+    elif new_status.isdigit():
+        number = int(new_status)
+        if 1 <= number <= 3:
+            status = statuses[number - 1]
+            print(f'\nСтатус заметки изменён на: {statuses[number - 1]}.')
+            break
+        else:
+            print(f'\nСтатус "{new_status}" не найден, повторите попытку.')
     else:
         print(f'\nСтатус "{new_status}" не найден, повторите попытку.')
 
-print(f'\nИмя пользователя: {note[0]} \n'
-      f'Заголовки заметки: \n'
-      f'{note[1]} \n'
-      f'Описание заметки: {note[2]} \n'
-      f'Статус заметки: {note[3]} \n'
-      f'Дата создания заметки: {note[4]} \n'
-      f'Дата дедлайна: {note[5]}')
+
+print(f'\nСтатус заметки: {status}')
